b6f1bea8495517cf460e6c9c566838ca9a689cbf
tategakibunko <lambda.watanabe@gmail.com>
2019-07-28 11:45:13 +0900

    Use filename if no toc is defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -185,6 +185,10 @@ export class AppComponent implements OnInit {
 
   get tocTitle(): string {
     if (!this.curToc) {
+      if (this.compileResult) {
+        const parts = this.compileResult.env.targetFilePath.split('/');
+        return parts[parts.length - 1] || 'TOC undefined';
+      }
       return 'TOC undefined';
     }
     return this.curToc.a.innerText;
