ac6b9088cff565a7d6359f22e5a71e01cf60799c
tategakibunko <lambda.watanabe@gmail.com>
2020-11-20 08:08:12 +0900

    Updated angular(10 -> 11)

--- src/app/bookmark-dialog/bookmark-dialog.component.spec.ts
+++ src/app/bookmark-dialog/bookmark-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BookmarkDialogComponent } from './bookmark-dialog.component';
 
@@ -6,7 +6,7 @@ describe('BookmarkDialogComponent', () => {
   let component: BookmarkDialogComponent;
   let fixture: ComponentFixture<BookmarkDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BookmarkDialogComponent ]
     })
